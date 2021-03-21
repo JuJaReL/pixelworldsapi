27f8a6da7fd5ab520b182eec83add70dc9b1dca4
zenqii <66673988+zenqii@users.noreply.github.com>
2021-03-21 14:09:58 +0800

    📗 Create setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,39 @@
-
-
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
 
 def get_long_description():
-  with open("README.md", "r") as f:
-    data = f.read()
-  return data
+        
+        with open("README.md", encoding="utf-8") as f:
+                readme = f.read()
+
+        return readme
 
 setup(
-  name="pixelworldsapi",
-  description="A python wrapper for Pixel Worlds' data",
+  name=__import__("pixelworlds").__title__,
+  author=__import__("pixelworlds").__author__,
+  description="🌎 A python wrapper for pixel worlds' data",
   long_description=get_long_description(),
   long_description_content_type='text/markdown',
-  install_requires=[
-    "beautifulsoup",
-    "requests"
-  ]
+  url="https://github.com/zenqii/pixelworldsapi",
+  version=__import__("pixelworlds").__version__,
+  python_requires=">=3.5",
+  platforms=["Windows"],
+  license=__import__("pixelworlds").__license__,
+  keywords=["api", "pixelworlds"],
   download_url="https://github.com/zenqii/pixelworldsapi/tarball/main",
+  zip_safe=False,
+  include_package_data=True,
+  install_requires = [
+	"beautifulsoup",
+	"requests"
+  ],
   classifiers=[
 		"Development Status :: 4 - Beta",
 		"Intended Audience :: Developers",
 		"Intended Audience :: Education",
-		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+		"License :: OSI Approved :: MIT License",
 		"Natural Language :: English",
-		"Operating System :: OS Independent",
+		"Operating System :: Microsoft :: Windows",
 		"Programming Language :: Python :: 3.5",
 		"Programming Language :: Python :: 3.6",
 		"Programming Language :: Python :: 3.7",
@@ -32,4 +42,3 @@ setup(
 		"Topic :: Software Development :: Libraries :: Python Modules",
 	]
 )
-
